60f159daec99ca6ed9a163740d97a2e19cf12c98
Leo_xh <1134086740@qq.com>
2019-06-15 13:23:17 +0800

    split enhance

--- src/enhance-global.cu
+++ src/enhance-global.cu
@@ -4,56 +4,6 @@
 #include <stdlib.h>
 
 // int updiv(int x, int y) { return (x + y - 1) / y; }
-
-__global__ void RGB2HSI(int * rgb_img, int * hsi_img, int height, int width)
-{
-    int img_x = __umul24(blockIdx.x, blockDim.x) + threadIdx.x,
-      img_y = __umul24(blockIdx.y, blockDim.y) + threadIdx.y;
-    int img_idx = __umul24(img_y, width) + img_x;
-    if (img_x < width && img_y < height) {
-        int R = rgb_img[img_idx] >> 16, G = (rgb_img[img_idx] >> 8) & 0x00FF, B = rgb_img[img_idx] & 0x0000FF;
-        float theta = acosf(((R-G+R-B)/2)/sqrtf(powf(R-G, 2)+(R-B)*(G-B)));
-        float H = (B <= G)? theta:2*CUDART_PI_F-theta;
-        H /= 2*CUDART_PI_F;
-        float S = 1 - 3.0*fminf(R,fminf(G, B))/(R+G+B);
-        float I = (R+G+B)/(3.0*255);
-        hsi_img[img_idx] = (int((H*255))<<16)+(int((S*255))<<8)+int((I*255));
-    }
-}
-__global__ void HSI2RGB(int * hsi_img, int * rgb_img, int height, int width)
-{
-    int img_x = __umul24(blockIdx.x, blockDim.x) + threadIdx.x,
-      img_y = __umul24(blockIdx.y, blockDim.y) + threadIdx.y;
-    int img_idx = __umul24(img_y, width) + img_x;
-    if (img_x < width && img_y < height) {
-        float H = (hsi_img[img_idx] >> 16)/255.0*2*CUDART_PI_F, S = ((hsi_img[img_idx] >> 8) & 0x00FF)/255.0, I = (hsi_img[img_idx] & 0x0000FF)/255.0;
-        int R, G, B;
-        if(H >= 0 && H < 2*CUDART_PI_F/3) {
-            B = I*(1-S)*255;
-            R = I*(1+S*cosf(H)/cosf(CUDART_PI_F/3-H))*255;
-            G = 3*I*255 - (R+B);
-            // printf("1 %d %d %d %f %f %f\n", R, G, B, H, S, I);
-        }
-        else if(H >= 2*CUDART_PI_F/3 && H < 4*CUDART_PI_F/3) {
-            H -= CUDART_PI_F/3*2;
-            R = I*(1-S)*255;
-            G = I*(1+S*cosf(H)/cosf(CUDART_PI_F/3-H))*255;
-            B = 3*I*255 - (R+G);
-            // printf("2 %d %d %d %f %f %f\n", R, G, B, H, S, I);
-        }
-        else if(H >= 4*CUDART_PI_F/3 && H < 2*CUDART_PI_F) {
-            H -= CUDART_PI_F/3*4;
-            G = I*(1-S)*255;
-            B = I*(1+S*cosf(H)/cosf(CUDART_PI_F/3-H))*255;
-            R = 3*I*255 - (G+B);
-            // printf("3 %d %d %d %f %f %f\n", R, G, B, H, S, I);
-        }
-        R = min(R,255);
-        G = min(G,255);
-        B = min(B,255);
-        rgb_img[img_idx] = (R << 16) + (G << 8) + B;
-    }
-}
 __global__ void CLAHEPre(int * hsi_img, int * g_frq, int height, int width)
 // the 'tile' size is the same with the block size, 1 block for 9 tile
 {
@@ -212,7 +162,7 @@ __global__ void CLAHEAft(int * hsi_img, int * g_frq, int height, int width)
 //     return true;
 // }
 
-int* imgCLAHE(int *src_img, int img_height, int img_width)
+int* imgCLAHE_Global(int *src_img, int img_height, int img_width)
 {
     int * d_rgb_img, * d_hsi_img, *d_g_frq;
     int * ret_img;
@@ -254,7 +204,7 @@ int main(int argc, char **argv) {
   }
   fclose(fp);
 
-  int *enhancedImg = imgCLAHE(src_img, img_height, img_width);
+  int *enhancedImg = imgCLAHE_Global(src_img, img_height, img_width);
   for (int i = 0; i < img_height*img_width; ++i)
   {
       printf("%d ", enhancedImg[i] >> 16);
--- src/enhance.h
+++ src/enhance.h
@@ -8,13 +8,17 @@
 #define THRESHOLD 64
 #define TILESIZE 16
 
-__device__ float * RGB2HSI(int * rgb_img, int height, int width);
-__device__ int * HSI2RGB(int * hsi_img, int height, int width);
-__global__ void CLAHE(int * img, int height, int width);
+texture<int, 2, cudaReadModeElementType> tex1;
+texture<int, 2, cudaReadModeElementType> tex2;
+
+__device__ void RGB2HSI(int *rgb_img, int height, int width);
+__device__ void HSI2RGB(int *hsi_img, int height, int width);
+__global__ void CLAHE(int *img, int height, int width);
 // the 'grid' size is 1/4 of the block size
+__global__ void CLAHEPre(int *hsi_img, int *g_frq, int height, int width);
+__global__ void CLAHEAft(int *hsi_img, int *g_frq, int height, int width);
 
-int* imgCLAHE(int *src_img, int img_height, int img_width);
+int *imgCLAHE(int *src_img, int img_height, int img_width);
+int *imgCLAHE_Global(int *src_img, int img_height, int img_width);
 
-
-
-#endif // ENHANCE_H
+#endif  // ENHANCE_H
